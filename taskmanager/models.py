d973a9e15557d8fd98ce85b45fa09a564654a9b9
Rick-8 <npopbwls@students.codeinstitute.net>
2025-02-24 13:55:43 +0000

    Add - Import db and create all()

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -4,7 +4,7 @@ from taskmanager import db
 class Category(db.Model):
     # schema for the category model
     id = db.Column(db.Integer, primary_key=True)
-    Category_name = db.Column(db.string(25), unique=True, nullable=False)
+    Category_name = db.Column(db.String(25), unique=True, nullable=False)
     tasks = db.relationship('Task', backref='category', cascade="all, delete", lazy=True) # noqa
 
     def __repr__(self):
@@ -16,10 +16,10 @@ class Task(db.Model):
     # schema for the task model
     id = db.Column(db.Integer, primary_key=True)
     Task_name = db.Column(db.String(50), unique=True, nullable=False)
-    Task_description = db.Column(db.text, nullable=False)
+    Task_description = db.Column(db.Text, nullable=False)
     is_urgent = db.Column(db.Boolean, default=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', onedelete="CASCADE"), nullable=False) # noqa
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete="CASCADE"), nullable=False) # noqa
 
     def __repr__(self):
         return "#{0} - Task {1} | Urgent: {2}".format(
