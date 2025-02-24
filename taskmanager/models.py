48c8e34c04eb43b0c9751ba952b3febb2a44e8b0
Rick-8 <npopbwls@students.codeinstitute.net>
2025-02-24 20:07:55 +0000

    Adding categorys section

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -2,26 +2,29 @@ from taskmanager import db
 
 
 class Category(db.Model):
-    # schema for the category model
+    # schema for the Category model
     id = db.Column(db.Integer, primary_key=True)
-    Category_name = db.Column(db.String(25), unique=True, nullable=False)
-    tasks = db.relationship('Task', backref='category', cascade="all, delete", lazy=True) # noqa
+    category_name = db.Column(db.String(25), unique=True, nullable=False)
+    tasks = db.relationship(
+        "Task", backref="category", cascade="all, delete", lazy=True)
 
     def __repr__(self):
-        # __repr__ to represent itself in thre form of a string
-        return self.Category_name
+        # __repr__ to represent itself in the form of a string
+        return self.category_name
 
 
 class Task(db.Model):
-    # schema for the task model
+    # schema for the Task model
     id = db.Column(db.Integer, primary_key=True)
-    Task_name = db.Column(db.String(50), unique=True, nullable=False)
-    Task_description = db.Column(db.Text, nullable=False)
-    is_urgent = db.Column(db.Boolean, default=False)
+    task_name = db.Column(db.String(50), unique=True, nullable=False)
+    task_description = db.Column(db.Text, nullable=False)
+    is_urgent = db.Column(db.Boolean, default=False, nullable=False)
     due_date = db.Column(db.Date, nullable=False)
-    category_id = db.Column(db.Integer, db.ForeignKey('category.id', ondelete="CASCADE"), nullable=False) # noqa
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        "category.id", ondelete="CASCADE"), nullable=False)
 
     def __repr__(self):
-        return "#{0} - Task {1} | Urgent: {2}".format(
-            self.id, self.Task_name, self.is_urgent
+        # __repr__ to represent itself in the form of a string
+        return "#{0} - Task: {1} | Urgent: {2}".format(
+            self.id, self.task_name, self.is_urgent
         )
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,6 +1,10 @@
-// sidenav Initialization
+// Sidenav Initialization with options
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-  });
+  var elems = document.querySelectorAll('.sidenav');
+  var options = {
+      edge: 'left', // Side from which the sidenav will open
+      draggable: true // Enable dragging to close the sidenav
+  };
+  M.Sidenav.init(elems, options);
+});
 
