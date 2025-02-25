5b11a57de891d6a24a7f0c615a048a19929ab1f6
Rick-8 <npopbwls@students.codeinstitute.net>
2025-02-25 17:39:45 +0000

    Add - .python-version

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,34 +1,23 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-
-# Initialize the app
-app = Flask(__name__)
-
-# Load the environment variables if the file exists
-if os.path.exists('env.py'):
+if os.path.exists("env.py"):
     import env  # noqa
 
-# Configure the app with secret key
-app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
-# Configure the SQLALCHEMY_DATABASE_URI based on the environment
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
-    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-        "DB_URL")  # Local database URL for development
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
     uri = os.environ.get("DATABASE_URL")
-    # Adjust for compatibility with the older 'postgres://' format
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
-    app.config["SQLALCHEMY_DATABASE_URI"] = uri  # Set the URI for production
+    app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
-# Initialize the database with the app
+
 db = SQLAlchemy(app)
 
-# Initialize Flask-Migrate with the app and db
-migrate = Migrate(app, db)
-
+from taskmanager import routes  # noqa
 # Import your routes
 from taskmanager import routes  # noqa
