291ff9bd507351b470630d28bd78a79aa627235e
Rick-8 <npopbwls@students.codeinstitute.net>
2025-02-25 16:34:08 +0000

    Add - All sections complete

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,14 +1,24 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-if os.path.exists('env.py'):
-    import env # noqa
+from flask_migrate import Migrate
 
-
+# Initialize the app
 app = Flask(__name__)
+
+# Load the environment variables if the file exists
+if os.path.exists('env.py'):
+    import env  # noqa
+
+# Configure the app with secret key and database URI
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+# Initialize the database with the app
 db = SQLAlchemy(app)
 
-from taskmanager import routes # noqa
+# Initialize Flask-Migrate with the app and db
+migrate = Migrate(app, db)
+
+# Import your routes
+from taskmanager import routes  # noqa
--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,10 +1,28 @@
-// Sidenav Initialization with options
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.sidenav');
+ // Sidenav Initialization with options
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
   var options = {
-      edge: 'left', // Side from which the sidenav will open
-      draggable: true // Enable dragging to close the sidenav
+    edge: "left", // Side from which the sidenav will open
+    draggable: true, // Enable dragging to close the sidenav
   };
   M.Sidenav.init(elems, options);
-});
 
+
+  // Datepicker initialization
+  var datepicker = document.querySelectorAll(".datepicker");
+  M.Datepicker.init(datepicker, {
+  format: "yyyy-mm-dd",
+  i18n: {
+    done: "Select"
+  }
+  });
+
+  // select initialization
+  let selects = document.querySelectorAll("select");
+  M.FormSelect.init(selects);
+
+  // collapsible initialization
+  var collapsibles = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(collapsibles);
+
+});
